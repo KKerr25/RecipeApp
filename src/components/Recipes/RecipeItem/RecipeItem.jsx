dd0ea05a2766096282986410ce43ed6c83af88c4
Konrad Kerr <konradkerr@mail.weber.edu>
2025-03-16 14:24:34 -0600

    wrong

--- src/components/Recipes/RecipeItem/RecipeItem.jsx
+++ src/components/Recipes/RecipeItem/RecipeItem.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import CartContext from "../../store/CartContext.jsx";
+import CartContext from "../store/CartContext.jsx";
 import Classes from "./Recipeitem.module.css";
 
 const RecipeItem = (props) => {
